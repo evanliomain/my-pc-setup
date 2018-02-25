8ae254c53ba48ab78d2c759515fb8496b8dfb4f5
Evan Liomain <evan.liomain@gmail.com>
2018-02-25 20:46:09 +0100

    Simplify mega install

--- packages/software/mega/install.sh
+++ packages/software/mega/install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+source ../../../functions.sh
+
 UBUNTU_VERSION=$(lsb_release -r | sed -e "s#Release:\t##")
 
 # Install some uncommon dependancies
 #sudo apt-get install -f libcrypto++9 libmediainfo0 libzen0
 
-wget https://mega.nz/linux/MEGAsync/xUbuntu_${UBUNTU_VERSION}/amd64/megasync-xUbuntu_${UBUNTU_VERSION}_amd64.deb
-#dpkg-deb --info megasync-xUbuntu_${UBUNTU_VERSION}_amd64.deb
-sudo dpkg -i megasync-xUbuntu_${UBUNTU_VERSION}_amd64.deb
-rm megasync-xUbuntu_${UBUNTU_VERSION}_amd64.deb
+install_deb "https://mega.nz/linux/MEGAsync/xUbuntu_${UBUNTU_VERSION}/amd64" "megasync-xUbuntu_${UBUNTU_VERSION}_amd64.deb"
