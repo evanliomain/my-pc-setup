8afec2738d7b8b9971ebb8f5bbc405098fb2e8ea
Evan Liomain <evan.liomain@gmail.com>
2018-02-25 21:12:28 +0100

    Install vscode extension during its configuration

--- packages/software/vscode/config.sh
+++ packages/software/vscode/config.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-$CONFIG_PATH=~/.config/Code/User
+CONFIG_PATH=~/.config/Code/User
 
 mkdir -p $CONFIG_PATH
 cp ./config/* $CONFIG_PATH
+
+for extension in `cat ./packages/software/vscode/extensions-list.txt`
+do
+  log "Install vscode extension $extension"
+  code --install-extension $extension
+done
